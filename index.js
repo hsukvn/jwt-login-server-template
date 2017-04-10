2a13e97f8845696814ea26e9c39e23d9c19ea42f
kevinhsu <kevinhsu@synology.com>
2017-04-11 00:39:24 +0800

    add POST signup

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router')
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost/auth');
 
 // App Setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
---
+++ router.js
@@ -0,0 +1,5 @@
+const Authentication = require('./controller/authentication');
+
+module.exports = function(app) {
+  app.post('/signup', Authentication.signup);
+}
