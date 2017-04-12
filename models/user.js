31c4927d78f188b20d77ff97e841d52b7f153814
kevinhsu <kevinhsu@synology.com>
2017-04-12 23:25:42 +0800

    encrypt the password

--- models/user.js
+++ models/user.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs')
 const Schema = mongoose.Schema;
 
 // Define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On Save Hook, encrypt password
+userSchema.pre('save', function(next) {
+  // get access to the user model
+  const user = this; // user.email, user.password
+
+  // generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 
